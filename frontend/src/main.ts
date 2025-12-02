57a86b0946a3e3308bd6741890af3aeec5a80a80
CedricTech <157019851+Cedric1024@users.noreply.github.com>
2025-12-02 12:39:21 +0800

    feat(cases): add two beautiful cases (#4)
    Co-authored-by: Cedric <yexiaoma@bupt.edu.cn>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// vue-router
 import {router} from "./router";
+// ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
