c231cac54fa4c4646ff9b736193699d159847fcf
Cedric <yexiaoma@bupt.edu.cn>
2025-12-07 12:15:49 +0800

    feat: add order page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
+import Order from "@/pages/Order.vue";
+import WeChatQR from "@/pages/WeChatQR.vue";
 
 const routes = [
-    { path: "/", name: "home", component: Home },
-    // 其他路由...
+  { path: "/", name: "home", component: Home },
+  { path: "/order", name: "order", component: Order },
+  { path: "/wechat", name: "wechat", component: WeChatQR },
+  // 其他路由...
 ];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
